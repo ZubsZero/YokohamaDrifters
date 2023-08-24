91135079490152e034b3a5f0d1b233b331f30cf4
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-08-24 11:27:17 +0200

    UpdatedApi

--- api/config/index.js
+++ api/config/index.js
@@ -4,7 +4,6 @@ const connection = createPool({
   host: process.env.dbHost,
   user: process.env.dbUser,
   password: process.env.dbPwd,
-  port: process.env.dbport,
   database: process.env.dbName,
   multipleStatements: true,
   connectionLimit: 30,
--- api/middleware/AuthenticateUser.js
+++ api/middleware/AuthenticateUser.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 function createToken(user) {
     return sign({
         emailAdd: user.emailAdd,
-        userPwd: user.userPwd
+        userPass: user.userPass
     },
     process.env.SECRET_KEY,
     {
